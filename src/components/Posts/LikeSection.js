2f235c5069f9b20b10764756a09f47c2aeb3850e
Johnny Murillo <jossters@gmail.com>
2021-03-17 09:09:52 -0700

    alldone

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -6,24 +6,26 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
-const LikeSection = props => {
+const LikeSection = (props) => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
-  const { likePost, numberOfLikes } = props;
+  const { id, likePost, numberOfLikes } = props;
 
   return (
     <div>
-      <div
-        className='like-section'
-        key='likes-icons-container'
-      >
+      <div className='like-section' key='likes-icons-container'>
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon
+            icon={faHeart}
+            onClick={() => {
+              likePost(id);
+            }}
+          />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
